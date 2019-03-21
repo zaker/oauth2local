da490e5c13ce0c14e12bed4c522795e6ee33b3e4
Sondre Naustdal <sondre.naustdal@gmail.com>
2019-03-21 16:07:56 +0100

    Handle url in windows

--- main.go
+++ main.go
@@ -1,43 +1,109 @@
 package main
 import (
+	"bytes"
+	"encoding/json"
+	"flag"
 	"fmt"
 	"log"
+	"net/http"
 	"net/url"
 	"os"
+	"time"
+
+	"github.com/zaker/oauth2local/register"
 
 	"github.com/joho/godotenv"
 	"github.com/pkg/browser"
 )
 
-const handleURLScheme = "loc-auth://"
+const handleURLScheme = "loc-auth"
 
-func redirectUri() string {
-	return handleURLScheme + "callback"
+var redirectCallback = flag.String("r", "", "Handles redirect from azure ad")
+
+func redirectURL() string {
+	return handleURLScheme + "://callback"
 }
 
 func authorizeURL(tenant, clientID, state string) string {
 	params := url.Values{}
 
-	params.Set("redirect_uri", redirectUri())
+	params.Set("redirect_uri", redirectURL())
 	params.Set("client_id", clientID)
+	params.Set("response_type", "code")
 	params.Set("state", state)
 	return fmt.Sprintf("https://login.microsoftonline.com/%s/oauth2/authorize?%s", tenant, params.Encode())
 }
 
 func tokenURL(tenant string) string {
+
 	return fmt.Sprintf("https://login.microsoftonline.com/%s/oauth2/token", tenant)
 }
 
-func main() {
-	log.Println(os.Args)
-	log.Println(os.Environ())
+func getToken(tokenURL, clientID, clientSecret, code string) (string, error) {
 
+	params := url.Values{}
+
+	params.Set("redirect_uri", redirectURL())
+	params.Set("client_id", clientID)
+	params.Set("client_secret", clientSecret)
+	params.Set("grant_type", "authorization_code")
+	params.Set("code", code)
+	params.Set("resource", clientID)
+	body := bytes.NewBufferString(params.Encode())
+	cli := new(http.Client)
+	resp, err := cli.Post(tokenURL, "application/x-www-form-urlencoded", body)
+	if err != nil {
+		return "", err
+	}
+	decoder := json.NewDecoder(resp.Body)
+	var dat map[string]interface{}
+	err = decoder.Decode(&dat)
+	if err != nil {
+		return "", err
+	}
+
+	accessToken := dat["access_token"].(string)
+	return accessToken, nil
+}
+
+func main() {
+	flag.Parse()
+
+	if len(*redirectCallback) > 0 {
+		// fmt.Print(*redirectCallback)
+		u, err := url.Parse(*redirectCallback)
+		if err != nil {
+			log.Println("Error parsing url", err, *redirectCallback)
+			time.Sleep(time.Second * 3)
+			return
+		}
+
+		if u.Scheme != handleURLScheme {
+			log.Println("We dont handle", u.Scheme)
+			time.Sleep(time.Second * 3)
+			return
+		}
+		params := u.Query()
+		code := params.Get("code")
+		accessToken, err := getToken(
+			tokenURL(os.Getenv("AAD_TENANT_ID")),
+			os.Getenv("AAD_CLIENT_ID"),
+			os.Getenv("AAD_CLIENT_SECRET"),
+			code)
+		if err != nil {
+			log.Println("Error parsing url", err)
+			time.Sleep(time.Second * 3)
+			return
+		}
+		fmt.Println(accessToken)
+		time.Sleep(time.Second * 3)
+		return
+	}
 	err := godotenv.Load()
 	if err != nil && !os.IsNotExist(err) {
 		log.Fatal("Error loading .env file", err)
 	}
 
-	log.Println(os.Environ())
-
+	register.RegMe(handleURLScheme, os.Args[0])
 	browser.OpenURL(authorizeURL(os.Getenv("AAD_TENANT_ID"), os.Getenv("AAD_CLIENT_ID"), "none"))
 }
--- register/register_win.go
+++ register/register_win.go
@@ -1,22 +1,86 @@
 package register
 
 import (
-	"fmt"
 	"log"
 
 	"golang.org/x/sys/windows/registry"
 )
 
-func RegMe() {
-	k, err := registry.OpenKey(registry.LOCAL_MACHINE, `SOFTWARE\Microsoft\Windows NT\CurrentVersion`, registry.QUERY_VALUE)
+// HKEY_CLASSES_ROOT
+//    alert
+//       URL Protocol = ""
+// Under this new key, the URL Protocol string value indicates that this key declares a custom pluggable protocol handler. Without this key, the handler application will not launch. The value should be an empty string.
+
+// Keys should also be added for DefaultIcon and shell. The Default string value of the DefaultIcon key must be the file name to use as an icon for this new URI scheme. The string takes the form "path, iconindex" with a maximum length of MAX_PATH. The name of the first key under the shell key should be an action verb, such as open. Under this key, a command key or a DDEEXEC key indicate how the handler should be invoked. The values under the command and DDEEXEC keys describe how to launch the application handling the new protocol.
+
+// Finally, the Default string value should contain the display name of the new URI scheme. The following example shows how to register an application, alert.exe in this case, to handle the alert scheme.
+
+// HKEY_CLASSES_ROOT
+//    alert
+//       (Default) = "URL:Alert Protocol"
+//       URL Protocol = ""
+//       DefaultIcon
+//          (Default) = "alert.exe,1"
+//       shell
+//          open
+//             command
+//                (Default) = "C:\Program Files\Alert\alert.exe" "%1"
+
+// Windows Registry Editor Version 5.00
+
+// [HKEY_CLASSES_ROOT\rtsp]
+// "URL Protocol"=""
+// @="URL:Rtsp Streaming Protocol"
+// "DefaultIcon"="\"C:\\Program Files (x86)\\VideoLAN\\VLC\\vlc.exe\",1"
+
+// [HKEY_CLASSES_ROOT\rtsp\shell]
+
+// [HKEY_CLASSES_ROOT\rtsp\shell\open]
+
+// [HKEY_CLASSES_ROOT\rtsp\shell\open\command]
+// @="\"C:\\Program Files (x86)\\VideoLAN\\VLC\\vlc.exe\" \"%1\""
+func RegMe(urlScheme ,locauthPath string) {
+	log.Println("Creating key " + urlScheme)
+	k, exist, err := registry.CreateKey(registry.CLASSES_ROOT, urlScheme, registry.WRITE)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer k.Close()
-
-	s, _, err := k.GetStringValue("SystemRoot")
+	if exist {
+		return
+	}
+	log.Println("Setting default value")
+	err = k.SetStringValue("", "URL:Handles aad tokens")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Windows system root is %q\n", s)
+	err = k.SetStringValue("URL Protocol", "")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = k.SetStringValue("URL Protocol", "")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Creating key", urlScheme+`\shell`)
+	k, exist, err = registry.CreateKey(registry.CLASSES_ROOT, urlScheme+`\shell`, registry.WRITE)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Creating key", urlScheme+`\shell\open`)
+	k, exist, err = registry.CreateKey(registry.CLASSES_ROOT, urlScheme+`\shell\open`, registry.WRITE)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Creating key", urlScheme+`\shell\open\command`)
+	k, exist, err = registry.CreateKey(registry.CLASSES_ROOT, urlScheme+`\shell\open\command`, registry.WRITE)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = k.SetStringValue("", "\""+locauthPath+"\" -r \"%1\"")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
