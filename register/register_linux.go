10e6414579ddb9c17ec471d65ee88f83c6ba8b77
Sondre Naustdal <sondre.naustdal@gmail.com>
2019-03-25 13:50:03 +0100

    Add build for linux support

--- register/register_linux.go
+++ register/register_linux.go
@@ -2,9 +2,46 @@ package register
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os/exec"
 )
 
-func RegMe() {
+const desktopHandlerTemplate = 
+`[Desktop Entry]
+Type=Application
+Name=Local Auth Scheme Handler
+Exec=%s -r \%u
+StartupNotify=false
+MimeType=x-scheme-handler/%s;`
+ 
+const xdgRegisterCommand = "xdg-mime default Auth2Local.desktop x-scheme-handler/%s"
 
-	fmt.Printf("Linux system root is %q\n", s)
+func regExist(newContent string) bool{
+	content, err := ioutil.ReadFile("testdata/hello")
+	if err != nil {
+		return false
+	}
+	return string(content) == newContent
+}
+
+
+func RegMe(urlScheme, locauthPath string) error {
+	fileBody  := fmt.Sprintf(desktopHandlerTemplate,locauthPath,urlScheme)
+
+	if regExist(fileBody){
+		return nil
+	}
+
+	filePath := "~/.local/share/applications/Auth2Local.desktop"
+	err  := ioutil.WriteFile(filePath,[]byte(fileBody), 0644)
+	if err != nil{
+		return err
+	}
+	cmd := exec.Command("xdg-mime","default","Auth2Local.desktop","x-scheme-handler/"+urlScheme)
+
+	err = cmd.Run()
+	if err != nil{
+		return err
+	}
+	return nil
 }
