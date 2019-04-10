131364a031496504350d2a6eaedf6a36378b27d8
Sondre Naustdal <sondre.naustdal@gmail.com>
2019-04-10 12:05:01 +0200

    Remove config command

--- cmd/config.go
+++ cmd/config.go
@@ -1,21 +1,21 @@
 package cmd
 
-import (
-	"github.com/spf13/cobra"
-	jww "github.com/spf13/jwalterweatherman"
-)
+// import (
+// 	"github.com/spf13/cobra"
+// 	jww "github.com/spf13/jwalterweatherman"
+// )
 
-// configCmd represents the config command
-var configCmd = &cobra.Command{
-	Use:   "config",
-	Short: "Shows the config settings",
-	Long:  `Shows the current congfig settings for the sovereign`,
-	Run: func(cmd *cobra.Command, args []string) {
-		jww.INFO.Println("config called")
-	},
-}
+// // configCmd represents the config command
+// var configCmd = &cobra.Command{
+// 	Use:   "config",
+// 	Short: "Shows the config settings",
+// 	Long:  `Shows the current congfig settings for the sovereign`,
+// 	Run: func(cmd *cobra.Command, args []string) {
+// 		jww.INFO.Println("config called")
+// 	},
+// }
 
-func init() {
-	rootCmd.AddCommand(configCmd)
+// func init() {
+// 	rootCmd.AddCommand(configCmd)
 
-}
+// }
