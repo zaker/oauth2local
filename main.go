84d825197680fb801f5add3b0a331840e19bd385
Sondre Naustdal <sondre.naustdal@gmail.com>
2020-01-07 13:34:52 +0100

    Decrease log treshold

--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"github.com/equinor/oauth2local/cmd"
+
+	jww "github.com/spf13/jwalterweatherman"
 )
 
 func main() {
+	jww.SetLogThreshold(jww.LevelTrace)
+	jww.SetStdoutThreshold(jww.LevelInfo)
 	cmd.Execute()
 }
