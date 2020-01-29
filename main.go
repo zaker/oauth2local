53291309d8883694f497fabaadb4b4a9db8786da
Sondre Naustdal <sondre.naustdal@gmail.com>
2020-01-29 13:02:52 +0100

    Set default log level to error

--- main.go
+++ main.go
@@ -2,12 +2,8 @@ package main
 
 import (
 	"github.com/equinor/oauth2local/cmd"
-
-	jww "github.com/spf13/jwalterweatherman"
 )
 
 func main() {
-	jww.SetLogThreshold(jww.LevelTrace)
-	jww.SetStdoutThreshold(jww.LevelInfo)
 	cmd.Execute()
 }
