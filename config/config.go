ff82ff4344b81494cdd860a9147e5761f14fbd6f
Sondre Naustdal <sondre.naustdal@gmail.com>
2020-01-30 13:09:01 +0100

    Better structure

--- config/config.go
+++ config/config.go
@@ -7,50 +7,51 @@ import (
 func SetDefaults() {
 	viper.SetEnvPrefix("O2L")
 
-	viper.RegisterAlias("CustomScheme", "CUSTOM_SCHEME")
 	viper.SetDefault("CUSTOM_SCHEME", "loc-auth")
-
-	viper.RegisterAlias("AuthServer", "AUTH_SERVER")
 	viper.SetDefault("AUTH_SERVER", "https://login.example.com/")
-
-	viper.RegisterAlias("AuthType", "AUTH_TYPE")
 	viper.SetDefault("AUTH_TYPE", "adal")
-
-	viper.RegisterAlias("TenantID", "TENANT_ID")
 	viper.SetDefault("TENANT_ID", "")
-
-	viper.RegisterAlias("ClientID", "CLIENT_ID")
 	viper.SetDefault("CLIENT_ID", "")
-
-	viper.RegisterAlias("ClientSecret", "CLIENT_SECRET")
 	viper.SetDefault("CLIENT_SECRET", "")
-
-	viper.RegisterAlias("ResourceID", "RESOURCE_ID")
 	viper.SetDefault("RESOURCE_ID", "")
+	viper.SetDefault("SCOPES", "")
 }
 
 func AuthServer() string {
+	viper.RegisterAlias("AuthServer", "AUTH_SERVER")
 	return viper.GetString("AUTH_SERVER")
 }
+
+func AuthServerType() string {
+	viper.RegisterAlias("AuthType", "AUTH_TYPE")
+	return viper.GetString("AUTH_TYPE")
+}
+
 func TenantID() string {
+	viper.RegisterAlias("TenantID", "TENANT_ID")
 	return viper.GetString("TENANT_ID")
 }
+
 func ClientID() string {
+	viper.RegisterAlias("ClientID", "CLIENT_ID")
 	return viper.GetString("CLIENT_ID")
 }
+
 func ClientSecret() string {
+	viper.RegisterAlias("ClientSecret", "CLIENT_SECRET")
 	return viper.GetString("CLIENT_SECRET")
 }
+
 func ResourceID() string {
+	viper.RegisterAlias("ResourceID", "RESOURCE_ID")
 	return viper.GetString("RESOURCE_ID")
 }
 
-func Scopes() string {
-	return viper.GetString("SCOPES")
+func Scopes() []string {
+	return viper.GetStringSlice("SCOPES")
 }
+
 func CustomScheme() string {
+	viper.RegisterAlias("CustomScheme", "CUSTOM_SCHEME")
 	return viper.GetString("CUSTOM_SCHEME")
-}
-func AuthServerType() string {
-	return viper.GetString("AUTH_TYPE")
 }
