cb5c22f36c5eb83a061ae3a56f830d3b3eeac456
Sondre Naustdal <sondre.naustdal@gmail.com>
2020-01-30 10:32:13 +0100

    Enabled environment variables, starting with 02L

--- config/config.go
+++ config/config.go
@@ -5,33 +5,52 @@ import (
 )
 
 func SetDefaults() {
-	viper.SetDefault("CustomScheme", "loc-auth")
-	viper.SetDefault("Authserver", "https://login.example.com/")
-	viper.SetDefault("AuthType", "adal")
-	viper.SetDefault("TenantID", "")
-	viper.SetDefault("ClientID", "")
-	viper.SetDefault("ClientSecret", "")
-	viper.SetDefault("ResourceID", "")
+	viper.SetEnvPrefix("O2L")
+
+	viper.RegisterAlias("CustomScheme", "CUSTOM_SCHEME")
+	viper.SetDefault("CUSTOM_SCHEME", "loc-auth")
+
+	viper.RegisterAlias("AuthServer", "AUTH_SERVER")
+	viper.SetDefault("AUTH_SERVER", "https://login.example.com/")
+
+	viper.RegisterAlias("AuthType", "AUTH_TYPE")
+	viper.SetDefault("AUTH_TYPE", "adal")
+
+	viper.RegisterAlias("TenantID", "TENANT_ID")
+	viper.SetDefault("TENANT_ID", "")
+
+	viper.RegisterAlias("ClientID", "CLIENT_ID")
+	viper.SetDefault("CLIENT_ID", "")
+
+	viper.RegisterAlias("ClientSecret", "CLIENT_SECRET")
+	viper.SetDefault("CLIENT_SECRET", "")
+
+	viper.RegisterAlias("ResourceID", "RESOURCE_ID")
+	viper.SetDefault("RESOURCE_ID", "")
 }
 
 func AuthServer() string {
-	return viper.GetString("Authserver")
+	return viper.GetString("AUTH_SERVER")
 }
 func TenantID() string {
-	return viper.GetString("TenantID")
+	return viper.GetString("TENANT_ID")
 }
 func ClientID() string {
-	return viper.GetString("ClientID")
+	return viper.GetString("CLIENT_ID")
 }
 func ClientSecret() string {
-	return viper.GetString("ClientSecret")
+	return viper.GetString("CLIENT_SECRET")
 }
 func ResourceID() string {
-	return viper.GetString("ResourceID")
+	return viper.GetString("RESOURCE_ID")
+}
+
+func Scopes() string {
+	return viper.GetString("SCOPES")
 }
 func CustomScheme() string {
-	return viper.GetString("CustomScheme")
+	return viper.GetString("CUSTOM_SCHEME")
 }
 func AuthServerType() string {
-	return viper.GetString("AuthType")
+	return viper.GetString("AUTH_TYPE")
 }
